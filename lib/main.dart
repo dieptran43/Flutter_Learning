18b232a424d5112d420050c675d54cfe6e1facb4
dieple <dieple@kootoro.com>
2018-10-05 14:55:07 +0700

    Build basic image, text and icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,62 @@
+/**
+ IPMAN
+ LE VAN DIEP
+ */
+
 import 'package:flutter/material.dart';
 
-void main()=>runApp(new MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return new MaterialApp(
-      title:'Flutter Demo',
-      theme: new ThemeData(primarySwatch: Colors.orange),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget{
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState()=> new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>{
-  int count =0;
-  void _incrementCounter(){
-    setState((){
-      count++;
-    });
-  }
-
-  @override
-  // TODO: implement widget
-  Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(10.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: new Text("Programming tutorials for you and for me.",
+                  style: new TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0
+                  ),
+                  )
+                ),
+                new Text("This is example tutorial videos in flutter",
+                  style: new TextStyle(
+                    color: Colors.grey[850],
+                    fontSize: 16.0
+                  )
+                ),
+              ],
+            ),
+          ),
+          new Icon(Icons.favorite, color: Colors.red),
+          new Text(" 200", style: new TextStyle(fontSize: 16.0))
+        ],
       ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    );
+
+    return new MaterialApp(
+      title: "",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Flutter App Image and Text")
+        ),
+        body: new ListView(
           children: <Widget>[
-            new Text("Hello cac bac, tong so lan nhap Add la: "),
-            new Text('$count', style: Theme.of(context).textTheme.display2,)
-          ]
+            new Image.asset('images/emca_01.jpeg',
+            fit: BoxFit.scaleDown),
+            titleSection
+          ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: "Nhan vao de Add",
-        child: new Icon(Icons.add_a_photo),
-      ),
     );
+
   }
 }
