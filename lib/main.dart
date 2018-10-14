067be7d5319900c5c8ba688132666939af8ca746
dieple <dieple@kootoro.com>
2018-10-14 23:49:31 +0700

    Add function girdview to show images

--- lib/main.dart
+++ lib/main.dart
@@ -2,89 +2,60 @@
  IPMAN
  LE VAN DIEP
  */
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() =>runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MainPage extends StatefulWidget{
+  final String title;
   @override
-  Widget build(BuildContext context) {
-    Widget titleSection = new Container(
-      padding: const EdgeInsets.all(10.0),
-      child: new Row(
-        children: <Widget>[
-          new Expanded(
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Container(
-                    padding: const EdgeInsets.only(bottom: 10.0),
-                    child: new Text(
-                      "Người đẹp siêu ngầu đến từ An Giang",
-                      style: new TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 18.0),
-                    )),
-                new Text("Cô ấy siêu lỳ và rất là dễ cưng, có điều hay khóc nhòe",
-                    style:
-                        new TextStyle(color: Colors.grey[850], fontSize: 16.0)),
-              ],
-            ),
-          ),
-          new Icon(Icons.favorite, color: Colors.red),
-          new Text(" 200", style: new TextStyle(fontSize: 16.0))
-        ],
+  MainPage({this.title}): super();
+
+  @override
+  State<StatefulWidget> createState(){
+    return new MainPageState();
+  }
+}
+
+class MainPageState extends State<MainPage>
+{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new GridView.extent(
+        maxCrossAxisExtent: 150.0,
+        mainAxisSpacing: 5.0,
+        crossAxisSpacing: 5.0,
+        padding: const EdgeInsets.all(5.0),
+        children: _buildGridTitles(29),
       ),
     );
+  }
+}
 
-    Widget buildButton(IconData icon, String buttonTitle) {
-      final Color tintColor = Colors.blue;
-      return new Column(
-        children: <Widget>[
-          new Icon(icon, color: tintColor),
-          new Container(
-              margin: const EdgeInsets.only(top: 5.0),
-              child: new Text(buttonTitle,
-                  style: new TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w600,
-                      color: tintColor))),
-        ],
-      );
-    }
-
-    Widget fourButtonsSection = new Container(
-      child: new Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          buildButton(Icons.home, "Home"),
-          buildButton(Icons.arrow_back, "Back"),
-          buildButton(Icons.arrow_forward, "Next"),
-          buildButton(Icons.share, "Share")
-        ],
+List<Widget> _buildGridTitles(numberOfTiles){
+  List<Container> containers = new List<Container>.generate(numberOfTiles, 
+  (int index){
+    final imageName = index < 9 ?'images/image0${index + 1}.JPG': 'images/image${index +1}.JPG';
+    return new Container(
+      child: new Image.asset(
+        imageName, fit: BoxFit.fill,
       ),
     );
+  });
+  return containers;
+}
 
-    final bottomTextSection = new Container(
-      padding: const EdgeInsets.all(20.0),
-      child: new Text(
-          "Đây là em Lê Diệu Phúc(Coca) biệt danh khác là Siêu Mè Nheo. Chiều cao: chân dài miên man(90cm). Cân nặng: 10kg. Số đo 3 vòng thì cực chuẩn....",
-          style: new TextStyle(color: Colors.grey[850], fontSize: 16.0)),
-    );
 
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
     return new MaterialApp(
       title: "",
-      home: new Scaffold(
-        appBar: new AppBar(title: new Text("Chương trình tìm siêu dễ cưng")),
-        body: new ListView(
-          children: <Widget>[
-            new Image.asset('images/emca_01.jpeg', fit: BoxFit.scaleDown),
-            titleSection,
-            fourButtonsSection,
-            bottomTextSection
-          ],
-        ),
-      ),
+      home: new MainPage(title:"GridView of images"),
     );
   }
 }
