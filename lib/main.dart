2a1e090b6708bf75dec988933c8086d145a36a09
dieple <dieple@kootoro.com>
2018-10-16 15:42:34 +0700

    Demo an snackbar example

--- lib/main.dart
+++ lib/main.dart
@@ -3,54 +3,39 @@
  LE VAN DIEP
  */
 import 'package:flutter/material.dart';
-
-void main() =>runApp(new MyApp());
+void main()=> runApp(new MyApp());
 
 class MyApp extends StatelessWidget{
+ final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  _showSnackBar(){
+    print('Show Snackbar here...');
+    final snackBar = new SnackBar(
+      content: new Text('This is a SnackBar'),
+      duration: new Duration(seconds: 3),
+      backgroundColor: Colors.black,
+      action: new SnackBarAction(label: "OK",onPressed: (){
+        print('Button OK cliked!!!');
+      },),
+    );
+    _scaffoldKey.currentState.showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context){
-    var card = new Card(
-      child: new Column(
-        children: <Widget>[
-          new ListTile(
-            leading: new Icon(Icons.account_box, color: Colors.blue, size: 26.0,),
-            title: new Text("Le Van Diep", style: new TextStyle(fontWeight: FontWeight.w400)),
-            subtitle: new Text("Sofware developer"),
-          ),
-          new Divider(color: Colors.blue,indent: 16.0),
-          new ListTile(
-            leading: new Icon(Icons.email, color: Colors.blue, size:26.0),
-            title: new Text("levandiep44@gmail.com", style: new TextStyle(fontWeight: FontWeight.w400),),
-          ),
-          new ListTile(
-            leading: new Icon(Icons.phone, color: Colors.blue, size: 26.0,),
-            title: new Text("+84-123456789", style: new TextStyle(fontWeight: FontWeight.w300),),
-          )
-        ],
-      ),
-    );
-
-    final sizeBox = new Container(
-      margin: new EdgeInsets.only(top: 30.0, left: 10.0, right: 10.0),
-      child: new SizedBox(
-        height: 220.0,
-        child: card,
-      ),
-    );
-
-    final center = new Center(
-      child: sizeBox,
-    );
-
     return new MaterialApp(
-      title: "",
+      title: 'SnackBar example',
       home: new Scaffold(
+        key: _scaffoldKey,
         appBar: new AppBar(
-          title: new Text("Card example"),
+          title: new Text('An example of SnackBar'),
+          actions: <Widget>[
+            new IconButton(
+              icon: new Icon(Icons.info),
+              onPressed: _showSnackBar,
+            )
+          ],
         ),
-        body: sizeBox,
       ),
-      
     );
   }
 }
