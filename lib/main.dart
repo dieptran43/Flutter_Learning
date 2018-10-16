05e21626755f85831777ac1aec9f20b849c11af3
dieple <dieple@kootoro.com>
2018-10-16 15:11:17 +0700

    Add ex about layout with Card and SizeBox

--- lib/main.dart
+++ lib/main.dart
@@ -4,78 +4,53 @@
  */
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
-
-class MainPage extends StatefulWidget{
-  final String title;
-  @override
-  MainPage({this.title}): super();
-
-  @override
-  State<StatefulWidget> createState(){
-    return new MainPageState();
-  }
-}
-
-class MainPageState extends State<MainPage>{
-    @override
-    Widget build(BuildContext context){
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text(widget.title),
-        ),
-        body: new GridView.extent(
-          maxCrossAxisExtent: 150.0,
-          mainAxisSpacing: 5.0,
-          crossAxisSpacing: 5.0,
-          padding: const EdgeInsets.all(5.0),
-          children: _buildGridTitles(29),
-        ),
-      );
-    }
-}
-
-List<Widget> _buildGridTitles(number){
-  List<Stack> ramdomLst = new List<Stack>.generate(number, 
-  (int index) {
-    final imageName = index <9?'images/image0${index +1}.JPG':'images/image${index +1}.JPG';
-    return new Stack(
-      alignment: const Alignment(0.0, 0.0),
-      children: <Widget>[
-        new Container(
-          child: new Image.asset(imageName, width: 150.0,height: 150.0,
-          fit: BoxFit.fill,),
-        ),
-        new Container(
-          padding: const EdgeInsets.all(5.0),
-          decoration: new BoxDecoration(
-            color: new Color.fromARGB(150, 71, 150, 236)
-          ),
-          child: new Text('${++index}', style: new TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.red
-          ),),
-        ),
-        new Container(
-          padding: const EdgeInsets.only(right: 0.0),
-          child: new Text('IP Test', style: new TextStyle(
-            fontSize: 28.0,
-            color: Colors.greenAccent[100]
-          ),),
-        )
-      ],
-    );
-  });
-  return ramdomLst;
-}
+void main() =>runApp(new MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    var card = new Card(
+      child: new Column(
+        children: <Widget>[
+          new ListTile(
+            leading: new Icon(Icons.account_box, color: Colors.blue, size: 26.0,),
+            title: new Text("Le Van Diep", style: new TextStyle(fontWeight: FontWeight.w400)),
+            subtitle: new Text("Sofware developer"),
+          ),
+          new Divider(color: Colors.blue,indent: 16.0),
+          new ListTile(
+            leading: new Icon(Icons.email, color: Colors.blue, size:26.0),
+            title: new Text("levandiep44@gmail.com", style: new TextStyle(fontWeight: FontWeight.w400),),
+          ),
+          new ListTile(
+            leading: new Icon(Icons.phone, color: Colors.blue, size: 26.0,),
+            title: new Text("+84-123456789", style: new TextStyle(fontWeight: FontWeight.w300),),
+          )
+        ],
+      ),
+    );
+
+    final sizeBox = new Container(
+      margin: new EdgeInsets.only(top: 30.0, left: 10.0, right: 10.0),
+      child: new SizedBox(
+        height: 220.0,
+        child: card,
+      ),
+    );
+
+    final center = new Center(
+      child: sizeBox,
+    );
+
     return new MaterialApp(
-      title: '',
-      home: new MainPage(title:'GridView of Images'),
+      title: "",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Card example"),
+        ),
+        body: sizeBox,
+      ),
+      
     );
   }
 }
